af4deb4a702fd8eef6163982a8317a1358642969
DH Suzon <diderhossainsuzon@gmail.com>
2025-04-27 22:10:26 +0600

    fix some code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const config = require("./config/config")
 const PORT = config.app.port;
-const host = "127.0.0.1";
+const host = "0.0.0.0";
 
 app.listen(PORT, host, () => {
   console.log(`server runing with on http://${host}:${PORT}`);
